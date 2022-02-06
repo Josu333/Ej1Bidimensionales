33dfcaf323ccacfc516470da27f66673c0665447
josu3 <josu3@JOSUEPC>
2022-02-06 18:52:33 +0100

    1er commit ejercicio finalizado

---
+++ src/ej1bidimensionales/Concesionario.java
@@ -0,0 +1,156 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej1bidimensionales;
+
+import Fecha.Fecha;
+import numeros.Numeros;
+import textos.Textos;
+
+/**
+ *
+ * @author dam
+ */
+public class Concesionario {
+
+    Modelo[] modelos;
+    String[] tipos;
+    float[] limites;
+    float[][] beneficios;
+
+    public void inicializar() {
+        tipos = new String[]{"Turismo", "Deportivo", "Todo Terreno"};
+        limites = new float[]{15000f, 30000f, 50000f,
+            100000f, Float.MAX_VALUE};
+        beneficios = new float[][]{
+            {0.15f, 0.10f, 0.10f, 0.18f, 0.06f},
+            {0.18f, 0.12f, 0.14f, 0.2f, 0.08f},
+            {0.21f, 0.14f, 0.16f, 0.22f, 0.1f}
+        };
+
+    }
+
+    public Concesionario() {
+        inicializar();
+        modelos = new Modelo[15];
+    }
+
+    public Concesionario(int numeroModelos) {
+        inicializar();
+        modelos = new Modelo[numeroModelos];
+    }
+
+    public void crear() {
+        for (int pos = 0; pos < modelos.length; pos++) {
+            modelos[pos] = new Modelo();
+        }
+    }
+
+    public void pedirDatos() {
+        String nombre;
+        int tipo;
+        for (int nmodelo = 0; nmodelo < modelos.length; nmodelo++) {
+            nombre = Textos.pedirString("Nombre del modelo: ");
+            menu();
+            tipo = Numeros.pedirNumero("Introduce un tipo:",
+                    0, tipos.length - 1);
+            modelos[nmodelo].grabar(nombre, tipo);
+        }
+    }
+
+    private void menu() {
+        for (int tipo = 0; tipo < tipos.length; tipo++) {
+            System.out.println(tipo + "\t" + tipos[tipo]);
+        }
+    }
+
+    public void pedirVentasMes() {
+        String[] meses = {"Enero",
+            "Febrero", "Marzo", "Abril",
+            "Mayo", "Junio"};
+        //float[] ventas;
+
+        float importe;
+        for (int nModelo = 0; nModelo < modelos.length;
+                nModelo++) {
+            //ventas=new float[6];
+            System.out.println("MODELO:" + modelos[nModelo].getNombre());
+            System.out.println("Ventas");
+            for (int mes = 0;
+                    mes < modelos[nModelo].getVentas().length;
+                    mes++) {
+                importe = Numeros.pedirNumeroReal(
+                        meses[mes] + "\t", 0);
+                modelos[nModelo].setUnaVenta(importe, mes);
+                // ventas[mes]= Numero.pedirNumeroReal(
+                //    meses[mes]+"\t",0);
+
+            }
+            // modelos[nModelo].setVentas(ventas);
+
+        }
+    }
+
+    public void informe() {
+        float total;
+        float beneficio;
+        int fila;
+        int columna;
+        int pagina=1;
+        cabecera(pagina);
+        int linea=3;
+        for (int nmodelo = 0; nmodelo < modelos.length; nmodelo++) {
+           if( linea==5){
+                pagina++;
+                cabecera(pagina);
+                linea=3;
+            }
+            System.out.print(modelos[nmodelo].getNombre());
+            System.out.print("\t\t" + modelos[nmodelo].ventasTrimestre(0));
+            System.out.print("\t\t" + modelos[nmodelo].ventasTrimestre(3));
+            total = modelos[nmodelo].ventasTrimestre(0)
+                    + modelos[nmodelo].ventasTrimestre(3);
+            System.out.print("\t\t" + total);
+            fila = modelos[nmodelo].getTipo();
+            columna = buscar(total);
+            beneficio = beneficios[fila][columna] * total;
+            System.out.println("\t" + beneficio);
+            linea++;
+            
+        }
+    }
+
+    private int buscar(float importe) {
+        int pos = 0;
+        while (importe >= limites[pos]) {
+            pos++;
+        }
+        /*
+        pos=0; 
+        boolean encontrado=false;
+        while( !encontrado && pos< limites.length)
+        {
+            if ( importe< limites[pos])
+                encontrado=true;
+            else
+                pos++;
+            
+        }
+        if ( !encontrado)
+            pos=-1;
+         */
+        return pos;
+    }
+
+    private void cabecera(int pagina) {
+        Fecha fhoy = new Fecha();
+
+        System.out.println("INFORME DE VENTAS");
+        System.out.println("Fecha:" + fhoy.fechaCompletaMesLetras()
+        +" Página:"+pagina);
+        System.out.println("MODELO \t IMPORTE 1TRI\t "
+                + "IMPORTE 2TRI \t TOTAL \t BENEFICIO");
+
+    }
+}
---
+++ src/ej1bidimensionales/Ej1Bidimensionales.java
@@ -0,0 +1,45 @@
+/*
+ Un concesionario de automóviles quiere guardar información de las ventas de sus
+ 15 modelos de coches durante los seis primeros meses.
+ Se pide:
+ a. Cargar los modelos de los coches (de un modelo se guarda el nombre del 
+ modelo y el tipo).
+ b. Cargar las ventas por meses.
+ c. Obtener un listado de ventas
+ Nota: El beneficio de las ventas se calcula en función del total de ventas y 
+tipo de coche.
+
+          IMPORTE VENTAS   15000   30000   50000   100000  +100000
+TURISMO
+Turismo                     15      10      10      18        6
+Deportivo                   18      12      14      20        8
+Todo Terreno                21      14      16      22       10
+
+
+                         INFORME DE VENTAS
+Fecha:                                                       Página:
+
+Modelo de coche | Importe ventas 1er trimestre | Importe ventas 2º trimestre | Total | Beneficio
+
+ */
+
+package ej1bidimensionales;
+
+/**
+ *
+ * @author dam
+ */
+public class Ej1Bidimensionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Concesionario miEmpresa=new Concesionario(3);
+        miEmpresa.crear();
+        miEmpresa.pedirDatos();
+        miEmpresa.pedirVentasMes();
+        miEmpresa.informe();
+    }
+    
+}
